836a833af53ee9e70e9d41cfd3e1b1041f4c49b9
mustafakemalbas <mustafa_kemalll@icloud.com>
2020-03-03 17:41:03 -0700

    Day 3 GoogleSearchTest

---
+++ src/test/java/day3/FindElementsPractice.java
@@ -0,0 +1,16 @@
+package day3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindElementsPractice {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+
+
+    }
+}
